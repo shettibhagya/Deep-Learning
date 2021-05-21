987271a51388c91ec6b09a198e2592330ebaef2a
Bhagyashri Shetti <75746037+shettibhagya@users.noreply.github.com>
2021-05-21 11:12:44 -0400

    Update dataset.py

--- YOLOv3/dataset.py
+++ YOLOv3/dataset.py
@@ -8,24 +8,20 @@ Original file is located at
 """
 
 import tensorflow as tf
-from absl.flags import FLAGS # This repository is a collection of Python library code for building Python applications. The code is collected from Google's own Python code base
-
-"""@tf.function is like an in-between version of eager execution and graph. You can step through the function by disabling tf.function and then gain performance when you enable it in production. You should not pass any non-tensor parameter to @tf.function, it will cause re-compilation on every call."""
+from absl.flags import FLAGS 
+# This repository is a collection of Python library code for building Python applications. 
 
 @tf.function
 def transform_targets_for_output(y_true, grid_size, anchor_idxs):
-    # y_true: (N, boxes, (x1, y1, x2, y2, class, best_anchor))
-    N = tf.shape(y_true)[0]
-
-    # y_true_out: (N, grid, grid, anchors, [x, y, w, h, obj, class])
-    y_true_out = tf.zeros(
-        (N, grid_size, grid_size, tf.shape(anchor_idxs)[0], 6))
-
+ 
+    y_true_out = tf.zeros((tf.shape(y_true)[0], grid_size, grid_size, tf.shape(anchor_idxs)[0], 6))
     anchor_idxs = tf.cast(anchor_idxs, tf.int32)
-
+    
+    N = tf.shape(y_true)[0]
     indexes = tf.TensorArray(tf.int32, 1, dynamic_size=True)
     updates = tf.TensorArray(tf.float32, 1, dynamic_size=True)
     idx = 0
+    
     for i in tf.range(N):
         for j in tf.range(tf.shape(y_true)[1]):
             if tf.equal(y_true[i][j][2], 0):
@@ -40,11 +36,9 @@ def transform_targets_for_output(y_true, grid_size, anchor_idxs):
                 anchor_idx = tf.cast(tf.where(anchor_eq), tf.int32)
                 grid_xy = tf.cast(box_xy // (1/grid_size), tf.int32)
 
-                # grid[y][x][anchor] = (tx, ty, bw, bh, obj, class)
-                indexes = indexes.write(
-                    idx, [i, grid_xy[1], grid_xy[0], anchor_idx[0][0]])
-                updates = updates.write(
-                    idx, [box[0], box[1], box[2], box[3], 1, y_true[i][j][4]])
+                
+                indexes = indexes.write( idx, [i, grid_xy[1], grid_xy[0], anchor_idx[0][0]])
+                updates = updates.write( idx, [box[0], box[1], box[2], box[3], 1, y_true[i][j][4]])
                 idx += 1
 
     return tf.tensor_scatter_nd_update( y_true_out, indexes.stack(), updates.stack())
@@ -57,11 +51,9 @@ def transform_targets(y_train, anchors, anchor_masks, size):
     anchors = tf.cast(anchors, tf.float32)
     anchor_area = anchors[..., 0] * anchors[..., 1]
     box_wh = y_train[..., 2:4] - y_train[..., 0:2]
-    box_wh = tf.tile(tf.expand_dims(box_wh, -2),
-                     (1, 1, tf.shape(anchors)[0], 1))
+    box_wh = tf.tile(tf.expand_dims(box_wh, -2),(1, 1, tf.shape(anchors)[0], 1))
     box_area = box_wh[..., 0] * box_wh[..., 1]
-    intersection = tf.minimum(box_wh[..., 0], anchors[..., 0]) * \
-        tf.minimum(box_wh[..., 1], anchors[..., 1])
+    intersection = tf.minimum(box_wh[..., 0], anchors[..., 0]) * \ tf.minimum(box_wh[..., 1], anchors[..., 1])
     iou = intersection / (box_area + anchor_area - intersection)
     anchor_idx = tf.cast(tf.argmax(iou, axis=-1), tf.float32)
     anchor_idx = tf.expand_dims(anchor_idx, axis=-1)
@@ -69,15 +61,14 @@ def transform_targets(y_train, anchors, anchor_masks, size):
     y_train = tf.concat([y_train, anchor_idx], axis=-1)
 
     for anchor_idxs in anchor_masks:
-        y_outs.append(transform_targets_for_output(
-            y_train, grid_size, anchor_idxs))
+        
+        y_outs.append(transform_targets_for_output( y_train, grid_size, anchor_idxs))
         grid_size *= 2
 
     return tuple(y_outs)
 
 def transform_images(x_train, size):
-    x_train = tf.image.resize(x_train, (size, size))
-    x_train = x_train / 255
+    x_train = tf.image.resize(x_train, (size, size)) / 255
     return x_train
 
 IMAGE_FEATURE_MAP = {
@@ -110,9 +101,9 @@ def parse_tfrecord(tfrecord, class_table, size):
     return x_train, y_train
 
 def load_tfrecord_dataset(file_pattern, class_file, size=416):
-    LINE_NUMBER = -1  # TODO: use tf.lookup.TextFileIndex.LINE_NUMBER
+
     class_table = tf.lookup.StaticHashTable(tf.lookup.TextFileInitializer(
-        class_file, tf.string, 0, tf.int64, LINE_NUMBER, delimiter="\n"), -1)
+        class_file, tf.string, 0, tf.int64, -1, delimiter="\n"), -1)
 
     files = tf.data.Dataset.list_files(file_pattern)
     dataset = files.flat_map(tf.data.TFRecordDataset)
@@ -120,8 +111,7 @@ def load_tfrecord_dataset(file_pattern, class_file, size=416):
 
 
 def load_fake_dataset():
-    x_train = tf.image.decode_jpeg(
-        open('./data/girl.png', 'rb').read(), channels=3)
+    x_train = tf.image.decode_jpeg(open('./data/girl.png', 'rb').read(), channels=3)
     x_train = tf.expand_dims(x_train, axis=0)
 
     labels = [
